ea2490badbbf40744c63e81425969e1b5da7bd43
logiclogue <jordanpaullord@hotmail.co.uk>
2017-11-05 20:54:00 +0000

    Adds id function

--- src/prelude.c
+++ src/prelude.c
@@ -16,6 +16,10 @@ Cons *prelude_minus(Cons *cons_a, Cons *cons_b) {
     return cons_from_int(int_a - int_b);
 }
 
+Cons *prelude_id(Cons *cons) {
+    return cons;
+}
+
 void prelude_test(void) {
     assert(cons_equal(
         prelude_add(cons_from_int(2), cons_from_int(3)),
@@ -23,4 +27,7 @@ void prelude_test(void) {
     assert(cons_equal(
         prelude_minus(cons_from_int(2), cons_from_int(3)),
         cons_from_int(-1)));
+    assert(cons_equal(
+        prelude_id(cons_from_string("add")),
+        cons_from_string("add")));
 }
--- src/prelude.h
+++ src/prelude.h
@@ -5,6 +5,7 @@
 
 Cons *prelude_add(Cons *cons_a, Cons *cons_b);
 Cons *prelude_minus(Cons *cons_a, Cons *cons_b);
+//Cons *prelude_id(Cons *cons);
 void prelude_test(void);
 
 #endif
