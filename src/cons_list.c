7a43aada79f1b107a5e1dc8d27aab8fb2cfd40b0
logiclogue <jordanpaullord@hotmail.co.uk>
2017-11-05 17:08:47 +0000

    Adds cons list length function

--- src/cons_list.c
+++ src/cons_list.c
@@ -18,13 +18,28 @@ Cons *cons_list_item(int n, Cons *cons) {
     return cons_list_item(n, cons->cdr);
 }
 
+static int _cons_list_length(int n, Cons *cons) {
+    if (cons_is_empty(cons_list_item(n, cons))) {
+        return n;
+    }
+
+    return _cons_list_length(n + 1, cons);
+}
+
+int cons_list_length(Cons *cons) {
+    return _cons_list_length(0, cons);
+}
+
 void cons_list_test(void) {
     Cons *list = cons_from_string("one  two three");
+    Cons *empty_list = cons_from_string("");
 
     assert(cons_equal(
         cons_list_item(1, list),
         cons_from_string("two")));
     assert(cons_equal(
-        cons_list_item(1, cons_from_string("")),
+        cons_list_item(1, empty_list),
         cons_from_string("")));
+    assert(cons_list_length(list) == 3);
+    assert(cons_list_length(empty_list) == 0);
 }
--- src/cons_list.h
+++ src/cons_list.h
@@ -4,6 +4,7 @@
 #include "cons.h"
 
 Cons *cons_list_item(int n, Cons *cons);
+int cons_list_length(Cons *cons);
 void cons_list_test(void);
 
 #endif
